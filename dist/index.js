5fc69588aabab30f2c2f539f0a031cadbc778824
abdulelhakim68@gmail.com <elhakimdev>
2022-09-15 01:33:50 +0700

    fix and optimizzing chart service

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.send = void 0;
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const socket_io_1 = require("socket.io");
 const mqtt_1 = __importDefault(require("mqtt"));
+const rxjs_1 = require("rxjs");
 dotenv_1.default.config();
 const mqttHost = process.env.MQTT_HOST;
 const mqttPort = process.env.MQTT_PORT;
@@ -23,22 +23,21 @@ const client = mqtt_1.default.connect(connectUrl, {
     keepalive: 10
 });
 const topic = process.env.MQTT_TOPIC;
-client.on('connect', () => {
-    console.log('Connected');
-    client.subscribe([topic], () => {
-        console.log(`Subscribe to topic '${topic}' on port ${mqttHost}:${mqttPort}`);
-    });
+const onConnect = (0, rxjs_1.fromEvent)(client, "connect");
+const onSubscribe = (0, rxjs_1.of)(client.subscribe([topic]));
+const onError = (0, rxjs_1.fromEvent)(client, "error");
+const onMessage = (0, rxjs_1.fromEvent)(client, "message");
+onConnect.subscribe(() => {
+    console.log("connected to broker");
 });
-client.on("close", () => {
-    console.log("connection close");
-    client.reconnect();
+onSubscribe.subscribe((observer) => {
+    console.log("Subcribe to topic", topic, connectUrl);
 });
-client.on('error', function (error) {
-    console.log("Unable to connect: " + error);
-    process.exit(1);
+onError.subscribe((observer) => {
+    console.log("Unable to connect", observer);
 });
 const app = (0, express_1.default)();
-const port = process.env.PORT;
+const port = process.env.APP_PORT;
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server with Socket.io Server');
 });
@@ -47,66 +46,35 @@ const server = app.listen(port, () => {
 });
 const io = new socket_io_1.Server(server, {
     cors: {
-        origin: process.env.APP_MODE === 'production' ? `http://${process.env.WEBSOCKET_CLIENT_PRODUCTION_HOST}:${process.env.WEBSOCKET_CLIENT_PRODUCTION_PORT}` : `http://${process.env.WEBSOCKET_CLIENT_HOST}:${process.env.WEBSOCKET_CLIENT_PORT}`
+        origin: process.env.APP_MODE === 'production' ?
+            `http://${process.env.WEBSOCKET_CLIENT_PRODUCTION_HOST}:${process.env.WEBSOCKET_CLIENT_PRODUCTION_PORT}` :
+            `http://${process.env.WEBSOCKET_CLIENT_HOST}:${process.env.WEBSOCKET_CLIENT_PORT}`
     }
 });
-// please note that we defined outer to perform hositing
-let defaulLocomotiveID = "default";
-let timeStart = new Date();
-let lastSec = 0;
-// first ly we need to listen to socket connection, everything start from socket, the broker is behind as a callback
-io.on("connection", (socket) => {
-    console.log('A client connected, socket id : ' + socket.id);
-    // thee process inside her is simultaneous, but are blocking mechanisme can be performed heere, since we defineed a outeer state constraint
-    // after socket get conected please listen to socket event first
-    socket.on("locoid-changed", (...args) => {
-        console.log("Select performed: need to changedd", ...args);
-        const emitedClientEvent = (JSON.parse(args[0])).name;
-        defaulLocomotiveID = emitedClientEvent;
+let locoId = undefined;
+const webSocketSource = (0, rxjs_1.of)(io);
+webSocketSource.subscribe((socket) => {
+    const WsConnection = (0, rxjs_1.of)(socket);
+    WsConnection.subscribe((client) => {
+        const onWsClientConnected = (0, rxjs_1.fromEvent)(client, "connection");
+        const onWsClientDisconnected = (0, rxjs_1.fromEvent)(client, "disconnected");
+        onWsClientConnected.subscribe((o) => {
+            const socket = o;
+            const onLocoChanged = (0, rxjs_1.fromEvent)(socket, "locoid-changed");
+            console.log(`Client ID : ${o.id} connected`);
+            // jalankan observaable keyika loco changed
+            onLocoChanged.subscribe((...args) => {
+                const emitedSelectionChangeId = (JSON.parse(args[0])).value;
+                locoId = emitedSelectionChangeId;
+            });
+            // @ts-ignore jalankan observable yang match dengan filter
+            onMessage.pipe((0, rxjs_1.filter)(([topic, payload]) => locoId === (JSON.parse(payload.toString())).locoid)).subscribe(([topic, payload]) => {
+                io.emit("forward-ws-message", payload.toString(), topic);
+            });
+        });
+        onWsClientDisconnected.subscribe((o) => {
+            console.log(`Client ID : ${o.id} disconnected`);
+        });
     });
-    // the action is perfomed async, so we can also listen broker suimulatnesously, 
-    client.on("message", (topic, payload) => {
-        console.log("listen all message coming from broker");
-        (0, exports.send)("forward-ws-message", payload);
-        // const messageObject = JSON.parse(payload.toString());
-        // // chek if the broker payload is a single object -> is meaning the broker just send a single message 
-        // if(typeof messageObject === 'object' && messageObject.locoid === defaulLocomotiveID){
-        //   console.log('forward single message to vue that matched the rule')
-        // io.emit("forward-ws-message", payload.toString(), topic)
-        // }
-        // console.log(typeof messageObject)
-    });
-    // // mas rian kaya gini cara ngelisten di nodejs dari event emit yang saya kirim di client nya 
-    // socket.on("locoid-changed", (...args) => {
-    //   console.log(JSON.parse(args[0]))
-    // })
 });
-const send = (event, payload) => {
-    const payloadObject = JSON.parse(payload.toString());
-    let timeRun = new Date(payloadObject.gpsdatetime);
-    // @ts-ignore
-    let diff = Math.abs(timeStart - timeRun);
-    let sec = Math.floor(diff / 1000);
-    // console.log(sec, lastSec);
-    if (sec > lastSec) {
-        lastSec = sec;
-        io.emit("forward-ws-message", payload.toString(), topic);
-        console.log("send");
-    }
-};
-exports.send = send;
-// client.on('message', (topic, payload) => {
-//     console.log(`got message from topic : ${topic}`)
-//     // io.emit("forward-ws-message", payload.toString(), topic)
-//     const payloadObject = JSON.parse(payload.toString());
-//     let timeRun = new Date(payloadObject.gpsdatetime);
-//     // @ts-ignore
-//     let diff = Math.abs(timeStart - timeRun);
-//     let sec = Math.floor(diff/1000);
-//     console.log(sec, lastSec);
-//     if(sec > lastSec){
-//       lastSec = sec;
-//       io.emit("forward-ws-message", payload.toString(), topic)
-//     }
-// })
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import mqtt from 'mqtt';
 import { argv } from 'process';
+import { from, fromEvent, map, Observable, of, Subscriber, pipe, filter } from 'rxjs';
+import { Console } from 'console';
+import { takeWhile } from 'rxjs-compat/operator/takeWhile';
 
 dotenv.config();
 
@@ -12,8 +15,6 @@ const clientId = process.env.MQTT_CLIENTID
 
 const connectUrl = `mqtt://${mqttHost}:${mqttPort}`
 
-
-  
 const client = mqtt.connect(connectUrl, {
   clientId,
   clean: true,
@@ -26,26 +27,25 @@ const client = mqtt.connect(connectUrl, {
 
 const topic = process.env.MQTT_TOPIC;
 
-client.on('connect', () => {
-  console.log('Connected')
-  client.subscribe([topic], () => {
-    console.log(`Subscribe to topic '${topic}' on port ${mqttHost}:${mqttPort}`)
-  })
+const onConnect = fromEvent(client, "connect");
+const onSubscribe = of(client.subscribe([topic]));
+const onError = fromEvent(client, "error");
+const onMessage = fromEvent(client, "message");
+
+onConnect.subscribe(() => {
+  console.log("connected to broker")
 })
 
-client.on("close", () => {
-  console.log("connection close")
-  client.reconnect();
+onSubscribe.subscribe((observer) => {
+  console.log("Subcribe to topic", topic, connectUrl);
 })
 
-client.on('error', function(error) {
-  console.log("Unable to connect: " + error);
-  process.exit(1);
-});
-
+onError.subscribe((observer) => {
+  console.log("Unable to connect", observer)
+})
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.APP_PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server with Socket.io Server');
@@ -57,75 +57,42 @@ const server = app.listen(port, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.APP_MODE === 'production' ? `http://${process.env.WEBSOCKET_CLIENT_PRODUCTION_HOST}:${process.env.WEBSOCKET_CLIENT_PRODUCTION_PORT}` : `http://${process.env.WEBSOCKET_CLIENT_HOST}:${process.env.WEBSOCKET_CLIENT_PORT}`
+    origin: process.env.APP_MODE === 'production' ? 
+    `http://${process.env.WEBSOCKET_CLIENT_PRODUCTION_HOST}:${process.env.WEBSOCKET_CLIENT_PRODUCTION_PORT}` : 
+    `http://${process.env.WEBSOCKET_CLIENT_HOST}:${process.env.WEBSOCKET_CLIENT_PORT}`
   }
 });
 
-// please note that we defined outer to perform hositing
-let defaulLocomotiveID : string = "default";
-let timeStart = new Date();
-let lastSec = 0;
+let locoId: any = undefined;
 
-// first ly we need to listen to socket connection, everything start from socket, the broker is behind as a callback
-io.on("connection", (socket) => {
-  console.log('A client connected, socket id : ' + socket.id)
-  // thee process inside her is simultaneous, but are blocking mechanisme can be performed heere, since we defineed a outeer state constraint
-  
-  // after socket get conected please listen to socket event first
-  socket.on("locoid-changed", (...args) => {
-    console.log("Select performed: need to changedd", ...args)
-    const emitedClientEvent = (JSON.parse(args[0])).name
-    defaulLocomotiveID = emitedClientEvent;
+const webSocketSource = of(io);
+webSocketSource.subscribe((socket) => {
+  const WsConnection = of(socket);
+  WsConnection.subscribe((client) => {
+    const onWsClientConnected     = fromEvent(client, "connection");
+    const onWsClientDisconnected  = fromEvent(client, "disconnected");
+
+    onWsClientConnected.subscribe((o) => {
+      const socket = (o as Socket);
+      const onLocoChanged = fromEvent(socket, "locoid-changed")
+
+      console.log(`Client ID : ${(o as Socket).id} connected`);
+
+      // jalankan observaable keyika loco changed
+      onLocoChanged.subscribe((...args) => {
+        const emitedSelectionChangeId = (JSON.parse((args[0] as string))).value;
+        locoId = emitedSelectionChangeId;
+      })
+
+      // @ts-ignore jalankan observable yang match dengan filter
+      onMessage.pipe(filter(([topic, payload]) => locoId === (JSON.parse(payload.toString())).locoid)).subscribe(([topic, payload]) => {
+        io.emit("forward-ws-message", payload.toString(), topic)
+      })
+    })
+
+    onWsClientDisconnected.subscribe((o) => {
+      console.log(`Client ID : ${(o as Socket).id} disconnected`)
+    })
+
   })
-
-  // the action is perfomed async, so we can also listen broker suimulatnesously, 
-  client.on("message", (topic, payload) => {
-    console.log("listen all message coming from broker")
-    send("forward-ws-message", payload)
-    // const messageObject = JSON.parse(payload.toString());
-    // // chek if the broker payload is a single object -> is meaning the broker just send a single message 
-    // if(typeof messageObject === 'object' && messageObject.locoid === defaulLocomotiveID){
-    //   console.log('forward single message to vue that matched the rule')
-      // io.emit("forward-ws-message", payload.toString(), topic)
-    // }
-    // console.log(typeof messageObject)
-  })
-
-  // // mas rian kaya gini cara ngelisten di nodejs dari event emit yang saya kirim di client nya 
-  // socket.on("locoid-changed", (...args) => {
-  //   console.log(JSON.parse(args[0]))
-  // })
-});
-
-export const send = (event: string, payload: Buffer) => {
-    const payloadObject = JSON.parse(payload.toString());
-    let timeRun = new Date(payloadObject.gpsdatetime);
-    // @ts-ignore
-    let diff = Math.abs(timeStart - timeRun);
-    let sec = Math.floor(diff/1000);
-
-    // console.log(sec, lastSec);
-
-    if(sec > lastSec){
-      lastSec = sec;
-      io.emit("forward-ws-message", payload.toString(), topic);
-      console.log("send")
-    }
-}
-
-// client.on('message', (topic, payload) => {
-//     console.log(`got message from topic : ${topic}`)
-//     // io.emit("forward-ws-message", payload.toString(), topic)
-
-//     const payloadObject = JSON.parse(payload.toString());
-//     let timeRun = new Date(payloadObject.gpsdatetime);
-//     // @ts-ignore
-//     let diff = Math.abs(timeStart - timeRun);
-//     let sec = Math.floor(diff/1000);
-
-//     console.log(sec, lastSec);
-//     if(sec > lastSec){
-//       lastSec = sec;
-//       io.emit("forward-ws-message", payload.toString(), topic)
-//     }
-// })
+})
